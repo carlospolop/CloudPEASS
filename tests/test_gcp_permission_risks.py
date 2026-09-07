dbd8300fe8a7ec0c7056640f14fa9e464014f22c
Carlos Polop <carlospolop@gmail.com>
2026-09-07 20:15:44 +0200

    Add live-tested GCP data and credential risks

--- tests/test_gcp_permission_risks.py
+++ tests/test_gcp_permission_risks.py
@@ -19,6 +19,7 @@ from src.sensitive_permissions.gcp import (
         "resourcemanager.projects.setIamPolicy",
         "container.pods.exec",
         "cloudbuild.builds.create",
+        "composer.environments.executeAirflowCommand",
         "secretmanager.versions.access",
     ],
 )
@@ -34,12 +35,17 @@ def test_direct_compromise_permissions_are_critical(permission):
         "artifactregistry.repositories.downloadArtifacts",
         "pubsub.subscriptions.consume",
         "run.routes.invoke",
+        "secretmanager.versions.add",
         "container.clusters.getCredentials",
         "bigtable.tables.mutateRows",
-        "logging.views.access",
         "iam.serviceAccounts.getOpenIdToken",
         "cloudkms.cryptoKeyVersions.destroy",
         "artifactregistry.repositories.uploadArtifacts",
+        "datastore.entities.create",
+        "datastore.entities.delete",
+        "datastore.entities.get",
+        "datastore.entities.update",
+        "redis.instances.getAuthString",
         "source.repos.update",
         "workflows.executions.create",
     ],
@@ -69,6 +75,7 @@ def test_data_plane_and_context_dependent_permissions_are_high(permission):
         "secretmanager.secrets.update",
         "storage.objects.update",
         "iam.roles.create",
+        "logging.views.access",
         "future.widgets.frobnicate",
     ],
 )
@@ -152,6 +159,13 @@ def test_legacy_combinations_do_not_promote_every_create_and_update():
                 "iam.serviceAccounts.getAccessToken",
             },
         ),
+        (
+            {"container.serviceAccounts.createToken"},
+            {
+                "container.clusters.getCredentials",
+                "container.serviceAccounts.createToken",
+            },
+        ),
     ],
 )
 def test_context_dependent_privesc_is_critical_only_when_complete(incomplete, complete):
@@ -171,6 +185,42 @@ def test_context_dependent_privesc_is_critical_only_when_complete(incomplete, co
 def test_only_updating_an_existing_custom_role_is_directly_critical():
     assert classify_permission("gcp", "iam.roles.create") == "medium"
     assert classify_permission("gcp", "iam.roles.update") == "critical"
+
+
+def test_cloud_sql_data_api_is_high_only_with_login_permission():
+    peass = CloudPEASS(
+        very_sensitive_combinations,
+        sensitive_combinations,
+        "GCP",
+        1,
+    )
+    execute_only = peass.analyze_sensitive_combinations(
+        {"cloudsql.instances.executeSql"}
+    )
+    assert execute_only["sensitive_perms"] == set()
+
+    complete = {"cloudsql.instances.executeSql", "cloudsql.instances.login"}
+    result = peass.analyze_sensitive_combinations(complete)
+    assert result["sensitive_perms"] == complete
+
+
+def test_logging_data_access_is_high_only_with_list_and_view_permissions():
+    peass = CloudPEASS(
+        very_sensitive_combinations,
+        sensitive_combinations,
+        "GCP",
+        1,
+    )
+    for incomplete in (
+        {"logging.logEntries.list"},
+        {"logging.views.access"},
+    ):
+        result = peass.analyze_sensitive_combinations(incomplete)
+        assert result["sensitive_perms"] == set()
+
+    complete = {"logging.logEntries.list", "logging.views.access"}
+    result = peass.analyze_sensitive_combinations(complete)
+    assert result["sensitive_perms"] == complete
 
 
 def test_bundled_exact_risk_rules_do_not_overlap():
